908fa06e86abdbdc28dd7ec7c0bbc2ed29181de4
oldpilluwu <fawwazamin29@gmail.com>
2021-06-02 14:15:25 +0600

    scrollview done

---
+++ lib/views/anime_display_view.dart
@@ -0,0 +1,39 @@
+import 'package:boku_gg/commons/controller.dart';
+import 'package:boku_gg/widgets/anime_gridview.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AnimeDisplayPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              child: Text(
+                "Popular Right Now",
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Expanded(
+              child: AnimeGridView(),
+            ),
+            Obx(() {
+              if (animeController.isLoading.value)
+                return Center(child: LinearProgressIndicator());
+              else
+                return Container(
+                  height: 0,
+                );
+            })
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/anime_thumbnail.dart
+++ lib/widgets/anime_thumbnail.dart
@@ -7,42 +7,40 @@ class AnimeThumbnail extends FloatingActionButton {
     required id,
     required String title,
     required VoidCallback onPressed,
-  }) :
-        super(
-          onPressed: onPressed,
-          child: SizedBox(
-            child: Stack(
-              fit: StackFit.expand,
-              children: <Widget>[
-                Image(
-                  image: NetworkImage(imageLink),
-                  alignment: Alignment.center,
-                  height: double.infinity,
-                  width: double.infinity,
-                  fit: BoxFit.fill,
-                ),
-
-                Container(
-                  padding: const EdgeInsets.all(5.0),
-                  alignment: Alignment.bottomCenter,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: <Color>[
-                        Colors.black.withAlpha(0),
-                        Colors.black12,
-                        Colors.black45
-                      ],
+  }) : super(
+            onPressed: onPressed,
+            child: SizedBox(
+              child: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  Image(
+                    image: NetworkImage(imageLink),
+                    alignment: Alignment.center,
+                    height: double.infinity,
+                    width: double.infinity,
+                    fit: BoxFit.fill,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(5.0),
+                    alignment: Alignment.bottomCenter,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: <Color>[
+                          Colors.black.withAlpha(0),
+                          Colors.black12,
+                          Colors.black,
+                        ],
+                      ),
+                    ),
+                    child: Text(
+                      title,
+                      style: TextStyle(color: Colors.white, fontSize: 16.0),
+                      textAlign: TextAlign.start,
                     ),
                   ),
-                  child: Text(title,
-                    style: TextStyle(color: Colors.white, fontSize: 16.0),
-                    textAlign: TextAlign.start,
-                  ),
-                ),
-              ],
-            ),
-          )
-      );
+                ],
+              ),
+            ));
 }
