a751d0a6a8ba7748e68f3bffe1faa628126575c4
azwadkm22 <azwadkm22@gmail.com>
2021-06-07 03:36:34 +0600

    Added Video Player Page (two extra widgets added to do this)  and connected it with the episode button, also added two new dependencies(video_player and wakelock).

---
+++ lib/widgets/video_player_widget.dart
@@ -0,0 +1,34 @@
+import 'package:boku_gg/widgets/video_player_overlay.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayerWidget extends StatelessWidget {
+  final VideoPlayerController controller;
+
+  const VideoPlayerWidget({
+    required this.controller,
+  }) : super();
+
+  @override
+  Widget build(BuildContext context) =>
+      controller != null && controller.value.isInitialized
+          ? Container(alignment: Alignment.center, child: buildVideo())
+          : Container(
+          child: Center(child: CircularProgressIndicator(),)
+      );
+
+  Widget buildVideo() => Stack(
+      children: <Widget>[
+        buildVideoPlayer(),
+        VideoPlayerOverlay(controller: controller),
+      ]
+  );
+
+  Widget buildVideoPlayer() => Center(
+    child: AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: VideoPlayer(controller)
+    ),
+  );
+
+}
